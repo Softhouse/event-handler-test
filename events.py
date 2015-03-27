8e025dca1d71ae8b51fdffe99b27a18390e7ab15
Ermin Hrkalovic <ermin.hrkalovic@softhouse.se>
2015-03-27 15:07:59 +0100

    added so that we get redirected at a successful post

--- events.py
+++ events.py
@@ -1,7 +1,19 @@
+import os
+from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
 app = Flask(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+
 
 @app.route('/')
 def start():
@@ -10,11 +22,14 @@ def start():
 @app.route('/event', methods=['POST', 'GET'])
 def event():
     if request.method == 'POST':
-        pass
+        flash('New entry was successfully posted')
+        flash(request.form['event_name'])
+        flash(request.form['event_date_text'])
     else:
         pass
-    return "Hello world"
+    return redirect(url_for('start'))
 
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run('0.0.0.0', 8888)
