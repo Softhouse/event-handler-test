088f7c87cc0fe07178e756e58279ed74752e7445
Ermin Hrkalovic <ermin.hrkalovic@softhouse.se>
2015-03-27 14:42:26 +0100

    added function for receiving new events

--- events.py
+++ events.py
@@ -4,8 +4,17 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def start():
     return render_template('event_admin.html')
+
+@app.route('/event', methods=['POST', 'GET'])
+def event():
+	print 'test'
+	if request.method == 'POST':
+		return 'Hello world'
+	else:
+		pass
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 8888)
