8c25648b7e2e02c25c2fcb26ca06d3c76dcd81a3
Ermin Hrkalovic <ermin.hrkalovic@softhouse.se>
2015-03-27 14:07:35 +0100

    changed template used for / and updated requirements.txt

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('event_admin.html', entries='')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
